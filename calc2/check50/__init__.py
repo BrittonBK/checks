d38ac89b6159c4a7c6bd5a6c3d6d0f42a5b6ad42
BrittonBK <britton@riverstone.io>
2018-10-30 08:38:01 -0500

    adding first checks

---
+++ calc2/check50/__init__.py
@@ -0,0 +1,23 @@
+from check50 import *
+
+class calc2(Checks):
+
+	@check()
+	def exists(self):
+		"""calc2.c exists"""
+		self.require("calc2.c")
+
+	@check("exists")
+	def compiles(self):
+		"""calc2.c compiles"""
+		self.spawn("clang -o greedy calc2.c -lcs50 -lm").exit(0)
+
+	@check("compiles")
+	def test_2_plus_3_minus_1(self):
+		"""input 2 + 3 - 1 yields 4"""
+		self.spawn("./calc2 2 + 3 - 1").stdout("4\n", "4\n").exit(0)
+
+	@check("compiles")
+	def test_2_plus_3_times_4(self):
+		"""input 2 + 3 * 4 yields 14"""
+		self.spawn("./calc2 2 + 3 * 4").stdout("14\n", "14\n").exit(0)

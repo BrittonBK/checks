e30e017a1edbff3c00e911b8419ae30780853ec4
BrittonBK <britton@riverstone.io>
2018-10-31 08:35:39 -0500

    more

--- calc2/check50/__init__.py
+++ calc2/check50/__init__.py
@@ -18,7 +18,7 @@ class calc2(Checks):
 		self.spawn("./calc2 2 + 3 - 1").stdout("4.000000\n", "4.000000\n").exit(0)
 
 	@check("compiles")
-	def test_2.44_plus_3.2_plus_4.9(self):
+	def test_244_plus_32_plus_49(self):
 		"""input 2.44 + 3.2 + 10.560000"""
 		self.spawn("./calc2 2.44 + 3.2 + 4.9").stdout("10.560000\n", "10.560000\n").exit(0)
 
@@ -28,6 +28,6 @@ class calc2(Checks):
 		self.spawn("./calc2 2 + 3 x 4").stdout("14.000000\n", "14.000000\n").exit(0)
 
 	@check("compiles")
-	def test_2_plus_3_minus_1(self):
+	def test_32_minus_5525_div_4(self):
 		"""input 32 - 55.25 / 4 yields -18.187500"""
 		self.spawn("./calc2 32 - 55.25 / 4").stdout("18.187500\n", "18.187500\n").exit(0)

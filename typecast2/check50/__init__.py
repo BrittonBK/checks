7e5a1c8e5b92cc79721862aabbda3a6a29e086c7
BrittonBK <britton@riverstone.io>
2018-11-02 06:47:02 -0500

    fix name with decimal

--- typecast2/check50/__init__.py
+++ typecast2/check50/__init__.py
@@ -13,6 +13,6 @@ class typecast2(Checks):
 		self.spawn("clang -o typecast2 typecast2.c -lcs50 -lm").exit(0)
 
 	@check("compiles")
-	def test_5.5(self):
+	def test_5-5(self):
 		"""input 5.5 yields 5.5, 5, and 15"""
 		self.spawn("./typecast2 5.5").stdout("5.500000\n5\n15\n", "5.500000\n5\n15\n").exit(0)

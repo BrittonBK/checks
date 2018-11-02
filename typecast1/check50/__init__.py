7d010502a9561aa433f8c6c5e93deca02686e582
BrittonBK <britton@riverstone.io>
2018-11-02 06:33:41 -0500

    correcting

--- typecast1/check50/__init__.py
+++ typecast1/check50/__init__.py
@@ -16,3 +16,18 @@ class typecast1(Checks):
 	def test_a(self):
 		"""input a yields 97\na\nAdding 3 letters gives you d"""
 		self.spawn("./typecast1 a").stdout("97\na\nAdding 3 letters gives you d\n", "97\na\nAdding 3 letters gives you d\n").exit(0)
+
+	@check("compiles")
+	def test_K(self):
+		"""input K yields 75\na\nAdding 3 letters gives you N"""
+		self.spawn("./typecast1 K").stdout("75\nK\nAdding 3 letters gives you N\n", "75\nK\nAdding 3 letters gives you N\n").exit(0)
+
+	@check("compiles")
+	def test_y(self):
+		"""input y yields 121\ny\nAdding 3 letters gives you b"""
+		self.spawn("./typecast1 y").stdout("121\ny\nAdding 3 letters gives you b\n", "121\ny\nAdding 3 letters gives you b\n").exit(0)
+
+	@check("compiles")
+	def test_X(self):
+		"""input X yields 88\nX\nAdding 3 letters gives you A"""
+		self.spawn("./typecast1 X").stdout("88\nX\nAdding 3 letters gives you A\n", "88\nX\nAdding 3 letters gives you A\n").exit(0)
